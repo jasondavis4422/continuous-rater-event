fd46eb7644db5e0a9072c6bf155ff64cdbb1530e
jasondavis4422 <jason.a.davis.25@dartmouth.edu>
2025-04-06 18:32:34 -0400

    Created files.

---
+++ src/pages/Complete.svelte
@@ -0,0 +1,23 @@
+<script>
+let code = 'C25ZG3BV'
+</script>
+
+
+<style>
+    .container {
+        text-align: center;
+        align-items: center;
+        width: 50%;
+        margin: 0 auto !important; 
+    }
+
+</style>
+
+<div class="container">
+    <h1>Thank you for your participation!</h1>
+    <p>Here is your confirmation code: {code}</p>
+    <p>You will use this to go back to Prolific and complete the confirmation.</p>
+        <p>.</p>
+    You have watched all possible movies. <strong>You may exit this study.</strong>
+</div>
+
